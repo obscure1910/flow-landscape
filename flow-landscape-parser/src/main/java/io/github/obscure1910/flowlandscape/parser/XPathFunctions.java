c8128cb942027db92df335c7358af20fdb694a5d
obscure1910 <obscure1910googlemail.com>
2023-05-09 16:01:53 +0200

    refactoring

--- flow-landscape-parser/src/main/java/io/github/obscure1910/flowlandscape/parser/XPathFunctions.java
+++ flow-landscape-parser/src/main/java/io/github/obscure1910/flowlandscape/parser/XPathFunctions.java
@@ -2,12 +2,8 @@ package io.github.obscure1910.flowlandscape.parser;
 
 import org.w3c.dom.Document;
 
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathFactory;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Optional;
 
 class XPathFunctions {
 
@@ -33,43 +29,6 @@ class XPathFunctions {
     @FunctionalInterface
     interface ThrowingFunction<T, R, E extends Exception> {
         R apply(T t) throws E;
-    }
-
-    private static class NamespaceResolver implements NamespaceContext {
-        //Store the source document to search the namespaces
-        private final Document sourceDocument;
-
-        private final Namespace[] namespaces = new Namespace[]{
-                new Namespace("jms", "http://www.mulesoft.org/schema/mule/jms")
-        };
-
-        private Optional<String> findReverse(String str) {
-            return Arrays.stream(namespaces)
-                    .map(ns -> ns.reverse(str))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .findFirst();
-        }
-
-        public NamespaceResolver(Document document) {
-            sourceDocument = document;
-        }
-
-        //The lookup for the namespace uris is delegated to the stored document.
-        public String getNamespaceURI(String prefix) {
-            return findReverse(prefix)
-                    .orElseGet(() -> sourceDocument.lookupNamespaceURI(prefix));
-        }
-
-        public String getPrefix(String namespaceURI) {
-            return findReverse(namespaceURI)
-                    .orElseGet(() -> sourceDocument.lookupPrefix(namespaceURI));
-        }
-
-        @SuppressWarnings("rawtypes")
-        public Iterator getPrefixes(String namespaceURI) {
-            return null;
-        }
     }
 
 }
